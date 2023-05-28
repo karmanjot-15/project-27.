6914ce25d25e94e9cc8348451fee3f4756fa221e
karmanjot-15 <133650916+karmanjot-15@users.noreply.github.com>
2023-05-27 23:40:43 -0700

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,34 @@
+class Ground 
+
+{
+  constructor(x, y, w, h) 
+  {
+    let options = {
+      friction: 0.3,
+      restitution: 0.6,
+      isStatic:true
+    };
+    
+    this.body = Bodies.rectangle(x, y, w, h, options);
+    this.w = w;
+    this.h = h;
+    World.add(world, this.body);
+    ground.display();
+  }
+  
+
+  display() {
+    var pos = this.body.position;
+    var angle = this.body.angle;
+
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    strokeWeight(1);
+    stroke(255);
+    fill(127);
+    rect(0, 0, this.w, this.h);
+    pop();
+  }
+}
